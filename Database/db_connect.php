0cc2cbf8701f2e6dbcb15273cac12c3ea4af4427
jottastudio <joseortiz066@hotmail.com>
2023-12-09 10:55:04 +0100

    adding database connection

---
+++ Database/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+// db_connect.php
+$servername = "127.0.0.1"; // Asume que el servidor de la base de datos está en la misma máquina
+$username = "root"; // El usuario de la base de datos
+$password = "my_root_password"; // La contraseña del usuario de la base de datos
+$dbname = "Spotters"; // El nombre de tu base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Chequear la conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
