3e64dc246ba91efb08e9a5792e70e820ae1296da
jottastudio <joseortiz066@hotmail.com>
2023-12-09 10:51:56 +0100

    adding connection to gitignore

--- Database/db_connect.php
+++ Database/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // db_connect.php
-$servername = "127.0.0.1"; // Asume que el servidor de la base de datos está en la misma máquina
+$servername = "127.0.0.1.1"; // Asume que el servidor de la base de datos está en la misma máquina
 $username = "root"; // El usuario de la base de datos
 $password = "my_root_password"; // La contraseña del usuario de la base de datos
 $dbname = "Spotters"; // El nombre de tu base de datos
