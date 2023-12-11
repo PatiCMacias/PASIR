8ca3c3bff0c0858f6faed7d56da2641af725acf8
DESKTOP-M8BT7ND\Patricia <pati.cmacias@gmail.com>
2023-12-11 18:19:41 +0100

    los cambios de usuario y pass

--- Database/db_connect.php
+++ Database/db_connect.php
@@ -1,11 +1,12 @@
 <?php
 $servername = "127.0.0.1"; // Asume que el servidor de la base de datos está en la misma máquina
-$username = "root"; // El usuario de la base de datos
-$password = "my_root_password"; // La contraseña del usuario de la base de datos
+$username = "pat"; // El usuario de la base de datos
+$password = "pat_pasir"; // La contraseña del usuario de la base de datos
 $dbname = "Spotters"; // El nombre de tu base de datos
+$port = 3308;
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Chequear la conexión
 if ($conn->connect_error) {
