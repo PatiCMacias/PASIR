7985bd48076c79f9963705eecdcd461b699b301f
DESKTOP-M8BT7ND\Patricia <pati.cmacias@gmail.com>
2023-12-14 12:34:52 +0100

    añadiendo las imagenes a la bbdd

---
+++ html/index.php
@@ -0,0 +1,59 @@
+<?php include './html/header.php'; ?>
+
+    <div class="welcome-section">
+        <h2>Bienvenidos</h2>
+        <p>Explora el mundo de la aviación a través de los ojos de una apasionada de fotografía aeronáutica y de la
+            aviación en general.</p>
+        <p>¿Sabes lo que es un spotter? Según <a
+                    href="https://es.wikipedia.org/wiki/Spotter#:~:text=Spotter%20puede%20referirse%20a%3A,francesa%20de%20miner%C3%ADa%20de%20textos.">Wikipedia</a>
+            podemos definirlo como una persona que observa y registra aviones, pero los que miramos al cielo cada vez
+            que escuchamos un motor, sabemos que es algo muy diferente.</p>
+    </div>
+
+    <div class="column">
+        <div class="gallery">
+            <h2>Galería Destacada</h2>
+            <p>En esta sección podrás ver las mejores capturas.</p>
+            <a href="html/galeria.php" class="boton">Entra y disfruta &pink;</a>
+        </div>
+
+        <div class="latest-news">
+            <h2>Últimas Noticias</h2>
+            <p>¿Quieres estar al día de las últimas novedades en aviación?</p>
+            <a href="https://aviaciondigital.com/" class="boton">Entérate de todo &pink;</a>
+        </div>
+        <div class="events">
+            <h2>Próximos Eventos</h2>
+            <nav>
+                <ul>
+                    <li><a href="https://ejercitodelaire.defensa.gob.es/EA/eae/eventos/calendario.html" class="boton">Ejército
+                            del Aire</a></li>
+                    <li><a href="https://www.eventseye.com/ferias/zst3_ferias_europa_aeronautica-espacio.html"
+                           class="boton">Ferias</a></li>
+                    <li><a href="https://www.europeanairshows.co.uk/" class="boton">European Air Shows</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="welcome-section">
+        <h2>Aerowebs y Spotters Destacados</h2>
+        <p>Aquí te mostramos el enlace a webs amigas y además, a los mejores spotters del panorama nacional. Podrás
+            adquirir sus productos y ver los consejos sobre configuración de cámara para obtener los mejores
+            disparos.</p>
+        <nav>
+            <ul>
+                <li><a href="https://ismaeljorda.com/" class="boton">Ismael Jordá</a></li>
+                <li><a href="https://www.instagram.com/jorgeguardia_photo/" class="boton">Jorge Guardia</a></li>
+                <li><a href="https://www.instagram.com/beatriz.m.blancas/" class="boton">Beatriz M Blancas</a></li>
+                <li><a href="https://www.instagram.com/yubephoto/" class="boton">Yube</a></li>
+                <li><a href="https://www.instagram.com/miguspotter/?hl=es" class="boton">Miguel García</a></li>
+                <li><a href="https://www.aviationcorner.net" class="boton">Aviation Corner</a></li>
+                <li><a href="https://aeronavesmilitaresespanolas.com/" class="boton">Aeronaves Militares Españolas</a>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+
+<?php include './html/footer.php'; ?>
